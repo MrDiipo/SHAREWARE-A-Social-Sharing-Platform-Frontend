6e189232c54d8b9736b65b3ba06cf095abd3091f
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-31 03:31:01 +0100

    Implement User Signup Page Component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {UserSignupPage} from './pages/UserSignupPage';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <UserSignupPage />,
   document.getElementById('root')
 );
 
@@ -15,3 +14,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
---
+++ src/pages/UserSignupPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UserSignupPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sign Up</h1>
+            </div>
+        )
+    }
+}
+
+export default UserSignupPage;
