bcfd252c48b47a584525675688d4d57758d934a0
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-01 02:10:20 +0100

    Handle Input Change in Signup Page

--- src/pages/UserSignupPage.jsx
+++ src/pages/UserSignupPage.jsx
@@ -1,21 +1,67 @@
 import React, { Component } from 'react'
 
 export class UserSignupPage extends Component {
+        
+        state = {
+            displayName : ' ' ,
+            username : ' ',
+            password: ' ',
+            repeatPassword: ' '
+        }
+     
+
+    onChangeDisplayName = (event) =>{
+        const value = event.target.value;
+        this.setState({
+            displayName : value
+        });
+    }
+
+    onChangeUsername = (event) =>{
+        const value = event.target.value;
+        this.setState({
+            username : value
+        });
+    }
+
+    onChangePassword = (event) =>{
+        const value = event.target.value;
+        this.setState({
+            password : value
+        });  
+    }
+    onChangeRepeatPassword = (event) =>{
+        const value = event.target.value;
+        this.setState({
+            repeatPassword : value
+        });  
+    }
+
     render() {
         return (
             <div>
                 <h1>Sign Up</h1>
                 <div>
-                    <input placeholder="Your display name"/>
+                    <input 
+                    placeholder="Your display name" 
+                    value={this.state.displayName}
+                       onChange={this.onChangeDisplayName} 
+                    />
                 </div>
                 <div>
-                    <input placeholder="Your username"/>
+                    <input placeholder="Your username"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}
+                    />
                 </div>
                 <div>
-                    <input placeholder="Your password" type="password"/>
+                    <input placeholder="Your password" type="password"
+                        value={this.state.password} onChange={this.onChangePassword}
+                    />
                 </div>
                 <div>
-                    <input placeholder="Repeat your password" type="password"/>
+                    <input placeholder="Repeat your password" type="password"
+                     value={this.state.repeatPassword} onChange={this.onChangeRepeatPassword}/>
                 </div>
                 <div>
                     <button>Sign Up</button>
--- src/pages/UserSignupPage.spec.jsx
+++ src/pages/UserSignupPage.spec.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import {UserSignupPage} from './UserSignupPage';
 
@@ -51,7 +51,49 @@ describe('UserSignupPage', () =>{
             const submitButton = container.querySelector('button');
             expect(submitButton).toBeInTheDocument();
         })
+    })
 
+    describe('Interactions', ()=>{
+
+        const changeEvent = (content) => {
+            return {
+                target : {
+                    value: content
+                }
+            }
+        };
+        
+        it('Sets the displayName', ()=>{
+            const {queryByPlaceholderText } = render(<UserSignupPage/>);
+            const displayNameInput = queryByPlaceholderText('Your display name');
+            
+            fireEvent.change(displayNameInput, changeEvent('my-display-name'));
+            expect(displayNameInput).toHaveValue('my-display-name');
+        })
+
+        it('Sets the username value into state', ()=>{
+            const {queryByPlaceholderText } = render(<UserSignupPage/>);
+            const userNameInput = queryByPlaceholderText('Your username');
+            
+            fireEvent.change(userNameInput, changeEvent('my-user-name'));
+            expect(userNameInput).toHaveValue('my-user-name');
+        })
+
+        it('Sets the password value into state', ()=>{
+            const {queryByPlaceholderText } = render(<UserSignupPage/>);
+            const passwordInput = queryByPlaceholderText('Your password');
+            
+            fireEvent.change(passwordInput, changeEvent('my-password'));
+            expect(passwordInput).toHaveValue('my-password');
+        })
+
+        it('Sets the repeat password value into state', ()=>{
+            const {queryByPlaceholderText } = render(<UserSignupPage/>);
+            const passwordInput = queryByPlaceholderText('Repeat your password');
+            
+            fireEvent.change(passwordInput, changeEvent('my-repeat-password'));
+            expect(passwordInput).toHaveValue('my-repeat-password');
+        })
     })
 
 })
